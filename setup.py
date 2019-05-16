f4a23f748c39adc9c38132b6a9c46116bf56fee1
Eric Cramer <cramerericm@gmail.com>
2019-05-16 16:52:47 +0200

    renamed package in setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="clockplot-cireric",
+    name="ptoclockplot",
     version="0.0.1",
     author="Eric Cramer",
     author_email="eric.cramer@curie.fr",
